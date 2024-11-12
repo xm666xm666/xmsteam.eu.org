cd41324ec0e7e9b9f9a1ff2776a20937d86deb47
xm666xm666 <130415238+xm666xm666@users.noreply.github.com>
2024-11-13 00:52:47 +0800

    Add files via upload

--- js/backToTop.js
+++ js/backToTop.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // backToTop.js
 document.addEventListener('DOMContentLoaded', function () {
     const backToTopButton = document.getElementById('back-to-top');
@@ -19,4 +20,27 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     };
   });
+=======
+// backToTop.js
+document.addEventListener('DOMContentLoaded', function () {
+    const backToTopButton = document.getElementById('back-to-top');
+  
+    // 当滚动时，显示或隐藏按钮
+    window.onscroll = function() {
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+      } else {
+        backToTopButton.style.display = "none";
+      }
+    };
+  
+    // 点击按钮返回顶部
+    backToTopButton.onclick = function() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // 平滑滚动
+      });
+    };
+  });
+>>>>>>> 9e288e7 (10.25-初版)
   
